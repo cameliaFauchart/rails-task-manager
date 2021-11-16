1ee819cd08577cf84f0fba33c0173f657d36c15a
Camelia Fauchart <cami.chani1@gmail.com>
2021-11-16 16:03:09 +0100

    tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,50 @@
+class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :destroy, :edit, :update]
+  def index
+  @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new # needed to instantiate the form_for
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to tasks_path(@task)
+  end
+
+  def show
+  end
+
+  def edit
+  
+  end
+
+  def update
+    @task.update(task_params)
+    
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+
+    
+    redirect_to tasks_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :details)
+  end
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
+end
